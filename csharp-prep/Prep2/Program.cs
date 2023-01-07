c3a13ca29e88b89f496bbf644a07e049a5ce39f0
lehig <97483848+lehig@users.noreply.github.com>
2023-01-07 12:45:35 -0700

    W01 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter in your grade: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letterGrade = "a";
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+
+        if (grade >= 70 )
+        {
+            Console.WriteLine("Congratulations on passing the course!");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("You can always do better next time!");
+        }
     }
 }
