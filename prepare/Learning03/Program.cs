bf0d419742338f8a11260ff2557c3793d35d3d2b
lehig <97483848+lehig@users.noreply.github.com>
2023-02-07 20:06:32 -0700

    learning is completed for learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.Write("Type in the top number: ");
+        int topNum = int.Parse(Console.ReadLine());
+        Console.Write("Type the bottom number: ");
+        int botNum = int.Parse(Console.ReadLine());
+
+        Fraction fraction = new Fraction(topNum, botNum);
+        Fraction fraction0 = new Fraction();
+        Fraction fraction1 = new Fraction(topNum);
+
+        
+        Console.WriteLine(fraction0.GetFractionString());
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
