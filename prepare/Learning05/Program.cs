dcdab1e33642fe0e42d595778d045ba1ead067cd
lehig <97483848+lehig@users.noreply.github.com>
2023-03-21 15:45:30 -0600

    learning05 completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Shape square = new Square("red", 4.5);
+        shapes.Add(square);
+        Shape circle = new Circle(2.8, "blue");
+        shapes.Add(circle);
+        Shape rectangle = new Rectangle("purple", 10.2, 90.3);
+        shapes.Add(rectangle);
+
+        Console.WriteLine("These the colors and areas of the shapes in the list: ");
+
+        foreach(Shape shape in shapes){
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
